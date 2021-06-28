0e4e23cdee080e8d949635dec14c065961464a37
williampalacios <mich.palacios.hdz@gmail.com>
2021-06-27 22:07:18 -0500

    RequestBody aanotation

--- src/main/java/com/example/restservice/controladores/user/UserController.java
+++ src/main/java/com/example/restservice/controladores/user/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.restservice.datos.dto.UserDto;
 
